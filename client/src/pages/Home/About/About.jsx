619bacbe6e2fa7d0db79ca12685d79012c1d0d89
Nasim <nasim.programmer24.com>
2024-05-06 11:26:25 +0600

    added about, services section and make a login page like as UI

---
+++ client/src/pages/Home/About/About.jsx
@@ -0,0 +1,35 @@
+import person from "../../../assets/images//about_us/person.jpg";
+import parts from "../../../assets/images//about_us/parts.jpg";
+const About = () => {
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <div className="lg:w-1/2 relative">
+          <img
+            src={person}
+            className="w-3/4 rounded-lg shadow-2xl"
+          />
+          <img
+            src={parts}
+            className="w-1/2 absolute right-5 top-1/2 rounded-lg border-8 border-white shadow-2xl"
+          />
+          </div>
+          <div className="lg:w-1/2 p-6">
+          <h3 className="text-3xl text-orange-500 font-bold">About Us</h3>
+            <h1 className="text-5xl font-bold">We are qualified & of experience in this field</h1>
+            <p className="py-6">
+            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don&apos;t look even slightly believable. 
+            </p>
+            <p className="pb-6">
+            the majority have suffered alteration in some form, by injected humour, or randomised words which don&apos;t look even slightly believable. 
+            </p>
+            <button className="btn btn-warning bg-red-500 border-none text-white">Get More Info</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- client/src/pages/Home/Home/Home.jsx
+++ client/src/pages/Home/Home/Home.jsx
@@ -1,10 +1,13 @@
+import About from "../About/About";
 import Banner from "../Banner/Banner";
+import Services from "../Services/Services";
 
 const Home = () => {
   return (
     <div>
       <Banner />
-      <h2>This is Home</h2>
+      <About/>
+      <Services/>
     </div>
   );
 };
